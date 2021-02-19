e0afeda374a529122ef7c9b7c0c0a12ee99c9ca5
MehmetUgur7 <77232030+MehmetUgur7@users.noreply.github.com>
2021-02-19 10:12:26 +0300

    13 Şubat ilk mola

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -1,4 +1,7 @@
-﻿using Entities.Concrete;
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -12,14 +15,22 @@ namespace WebAPI.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        //Loosely coupled
+        //Naming convention
+        //IoC Container -- Inversion of Control
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
         [HttpGet]
         public List<Product> Get()
         {
-            return new List<Product>
-            {
-                new Product{ProductId=1, ProductName="Elma"},
-                new Product{ProductId=2, ProductName="Armut"},
-            };
+            //Dependency chain-            
+            var result = _productService.GetAll();
+            return result.Data;
         }
     }
 }
